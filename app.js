5ad96d947d9088e99977239d7faec0d283407e84
Jung Jung <watermmm12@gmail.com>
2016-09-10 23:07:01 +0900

    delete useless comment and rename pages(login->returning)

--- app.js
+++ app.js
@@ -10,39 +10,34 @@ var fortunes = [
 const express = require('express');
 var app = express();
 
-// static 미들웨어, 정적 자원을 담고 있는 디렉터리를 지목해서
-// 특별한 처리 없이 클라이언트에 전송. 이미지, css, js-client 등
-app.use(express.static(__dirname + '/public'));
-app.use(require('body-parser').urlencoded({extended: true}));
+// static 미들웨어, 정적 자원 디렉토리 지정
+app.use(express.static(__dirname + '/public'))
+   .use(require('body-parser').urlencoded({extended: true}));
 
 // 뷰 엔진 핸들바 설정
 const handlebars = require('express-handlebars').create({defaultLayout:'main'});
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine)
+   .set('view engine', 'handlebars');
 
 // 애플리케이션 포트 지정
 // res.writeHead 대용
 app.set('port', process.env.PORT || 3001);
 
 
-///////////라우팅///////////////
-
+/***************라우팅*******************/
 app.get('/', function(req, res){
  // 뷰 엔진에서 콘텐츠 타입 text/html과 상태 코드 200을 반환하므로 명시하지 않는다.
- // var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
- // res.render('login', {fortune: randomFortune});
  res.render('home');
 });
 
-app.get('/login', function(req, res){
- res.render('login', {layout: 'none'});
+app.get('/returning', function(req, res){
+ res.render('returning', {layout: 'none'});
+})
+app.post('/returning', function(req, res) {
+  console.log(req.body)
+  res.redirect('/returning');
 });
-//////////////////////////////////
-
-app.post('/login', function(req, res) {
- console.log(req.body)
- res.redirect('/login');
-});
+/***************************************/
 
 
 //////////////핸들러//////////////
@@ -55,14 +50,14 @@ app.post('/login', function(req, res) {
 app.use(function(req, res){
  //res.writeHead 대용
  res.status(404);
- res.render('404');
+ res.render('404', {layout: 'none'});
 });
 
 // 500 에러 페이지
 app.use(function(err, req, res, next){
  console.error(err.stack);
  res.status(500);
- res.render('500');
+ res.render('500', {layout: 'none'});
 });
 
 //////////////////////////////////
