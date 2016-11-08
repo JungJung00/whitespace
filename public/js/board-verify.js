abf6d526bb4984b9bd26f35ff775820b20b4546e
Jung Jung <watermmm12@gmail.com>
2016-11-08 11:48:20 +0900

    fix detail about scroll top

--- public/js/board-verify.js
+++ public/js/board-verify.js
@@ -7,8 +7,10 @@ function boardVerify(title){
     data: {'brd-title': title},
     dataType: 'json',
     success: function(data){
-      if(data.Opened)
+      if(data.Opened){
+        $('#viewpost-template-target').empty();
         pagingP(title, 1);
+      }
       else{
         $.ajax({
           url: '/js/template/board-verify-template.handlebars',
