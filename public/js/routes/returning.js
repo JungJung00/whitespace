a6b4874d5331828f3b74aee63ed2e7bfd124de4b
Jung Jung <watermmm12@gmail.com>
2016-11-12 21:23:29 +0900

    4 : modify url of returning

--- public/js/routes/returning.js
+++ public/js/routes/returning.js
@@ -1,11 +1,11 @@
 module.exports = function(app, passport){
   // 로그인 인증 과정을 passport에 위탁
-  app.post('/returning', passport.authenticate('local', {successRedirect: '/',
+  app.post('/outside/returning', passport.authenticate('local', {successRedirect: '/',
                                                                  failureRedirect: '/returning',
                                                                  failureFlash: false})
   );
 
-  app.get('/returning', function(req, res){
+  app.get('/outside/returning', function(req, res){
     // 이미 세션이 등록되어 있어 req에 user 객체가 있는 경우 home으로 이동한다.
     if(req.user){
       console.log('there is a session');
