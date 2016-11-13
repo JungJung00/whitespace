14ba9c3be0fade88159f8cb883eb824aa68a6f4c
Jung Jung <watermmm12@gmail.com>
2016-11-13 12:22:17 +0900

    cannot find module error

---
+++ credentials.js
@@ -0,0 +1,11 @@
+module.exports = {
+ cookieSecret: 'underline^SUGGESTED^sometimes^COPENHAGEN^paragraph^SINGAPORE^DIFFERENCE^understand^something^DEVELOPED',
+ gmail:{
+   user: 'watermmm12@gmail.com',
+   password: 'ehEHgks!!123'
+ },
+ postimage:{
+   user: 'watermmm12@gmail.com',
+   password: 'ehehgks!!123'
+ }
+}
